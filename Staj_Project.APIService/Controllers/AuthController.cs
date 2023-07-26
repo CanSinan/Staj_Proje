ea34955b7fb65cfa9014e3537a18b60cc92aca28
CanSinan <pmsinancan@gmail.com>
2023-07-26 09:43:11 +0300

    Identity Kısmı bitti, Database ve program.cs ayarları yapıldı

---
+++ Staj_Project.APIService/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Staj_Project.APIService.Services.IServices;
+using Staj_Project.Identity.Core.Dtos;
+
+namespace Staj_Project.APIService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+
+        }
+
+
+        [HttpPost]
+        [Route("seed-roles")]
+        public async Task<IActionResult> SeedRoles()
+        {
+            var seerRoles = await _authService.SeedRolesAsync();
+
+            return Ok(seerRoles);
+        }
+
+        [HttpPost]
+        [Route("CustomerRegister")]
+        public async Task<IActionResult> CustomerRegister([FromBody] RegisterDto model)
+        {
+            var registerResult = await _authService.CustomerRegisterAsync(model);
+
+            if (registerResult.IsSucceed)
+                return Ok(registerResult);
+            return BadRequest(registerResult);
+        }
+
+
+        [HttpPost]
+        [Route("ExpertRegister")]
+        public async Task<IActionResult> ExpertRegister([FromBody] RegisterDto model)
+        {
+            var registerResult = await _authService.ExpertRegisterAsync(model);
+
+            if (registerResult.IsSucceed)
+                return Ok(registerResult);
+
+            return BadRequest(registerResult);
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto model)
+        {
+            var loginResult = await _authService.LoginAsync(model);
+
+            if (loginResult.IsSucceed)
+                return Ok(loginResult);
+
+            return Unauthorized(loginResult);
+
+
+        }
+
+    }
+}
