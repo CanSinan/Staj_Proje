713dd1cd18fe65e62e106a1927a26196496e2df1
CanSinan <pmsinancan@gmail.com>
2023-07-26 09:28:13 +0300

    Identity i Classlibrary olarak oluşturuyoruz

---
+++ Staj_Project.Identity/Core/Dtos/RegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staj_Project.Identity.Core.Dtos
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "UsernName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ Staj_Project.Identity/OtherObject/StaticUserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staj_Project.Identity.OtherObject
+{
+    public class StaticUserRoles
+    {
+        public const string EXPERT = "EXPERT";
+        public const string CUSTOMER = "CUSTOMER";
+    }
+}
